165f7f51ffcd196889c6811c6e6b671d458c903b
bettyabay <88900241+bettyabay@users.noreply.github.com>
2022-10-15 16:52:37 +0300

    Time: 77 ms (88.53%), Space: 45.3 MB (5.74%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function deleteDuplicates(firstNode) {
+	const head = { next: firstNode };
+	let currentNode = head;
+
+	while (twoNodesFollow(currentNode)) {
+		if (twoDuplicatesFollow(currentNode)) {
+			const secondOfTheDuplicates = currentNode.next.next;
+			currentNode.next = getNextDistinctNode(secondOfTheDuplicates) || null;
+		} else {
+			currentNode = currentNode.next;
+		}
+	}
+
+	return head.next;
+}
+
+function twoNodesFollow(node) {
+	const bothNodesExist = node && node.next && node.next.next;
+	return bothNodesExist;
+}
+
+function twoDuplicatesFollow(node) {
+	const nextValue = node.next.val;
+	const nextNextValue = node.next.next.val;
+	return nextValue === nextNextValue;
+}
+
+function getNextDistinctNode(node) {
+	let currentNode = node;
+	const duplicateValue = node.val;
+
+	while (true) {
+		const isNull = currentNode === null;
+		const distinctNodeFound = currentNode && currentNode.val !== duplicateValue;
+
+		if (isNull) return undefined;
+		else if (distinctNodeFound) return currentNode;
+		else currentNode = currentNode.next;
+	}
+}
